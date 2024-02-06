5c148f00477c826d2f00cb0fb0c694c4b95c51c0
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-06 00:50:22 -0400

    :rocket: feat: add section hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
 
-function styles() {
+function compileStyles() {
   return gulp
     .src("./src/styles/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
-exports.watch = function(){
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function optimizeImages() {
+  return gulp
+    .src("./src/assets/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./dist/assets"));
 }
+
+function watchFiles() {
+  gulp.watch("./src/styles/**/*.scss", compileStyles);
+  gulp.watch("./src/assets/**/*", optimizeImages);
+}
+
+exports.default = gulp.parallel(compileStyles, optimizeImages);
+exports.watch = watchFiles;
