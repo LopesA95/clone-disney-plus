a95dc29f10ce2a2c454c0d6150e756cb210765bc
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-06 02:17:04 -0400

    :tada: feat: add sections shows and JS functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const browserSync = require("browser-sync").create();
 
 function compileStyles() {
   return gulp
     .src("./src/styles/*.scss")
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream()); // Stream changes to browserSync for CSS injection
 }
 
 function optimizeImages() {
@@ -17,8 +19,15 @@ function optimizeImages() {
 }
 
 function watchFiles() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+
   gulp.watch("./src/styles/**/*.scss", compileStyles);
   gulp.watch("./src/assets/**/*", optimizeImages);
+  gulp.watch("./*.html").on("change", browserSync.reload); // Reload on HTML file changes
 }
 
 exports.default = gulp.parallel(compileStyles, optimizeImages);
