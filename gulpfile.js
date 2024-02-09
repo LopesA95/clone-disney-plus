1c90bbb231b25e272094882819629f1d10e7b2bc
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-08 23:01:23 -0400

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
 const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify");
+
+function scripts() {
+  return gulp
+    .src("./src/scripts/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/scripts"));
+}
 
 function compileStyles() {
   return gulp
@@ -28,7 +36,8 @@ function watchFiles() {
   gulp.watch("./src/styles/**/*.scss", compileStyles);
   gulp.watch("./src/assets/**/*", optimizeImages);
   gulp.watch("./*.html").on("change", browserSync.reload); // Reload on HTML file changes
+  gulp.watch("./src/scripts/*.js", compileStyles);
 }
 
-exports.default = gulp.parallel(compileStyles, optimizeImages);
+exports.default = gulp.parallel(compileStyles, optimizeImages, scripts);
 exports.watch = watchFiles;
